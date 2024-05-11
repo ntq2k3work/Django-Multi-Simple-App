f4d6a170992cd1a5f47902ef0a9705e4ba72017b
ntq2k3work <ntq2k3.work@gmail.com>
2024-05-11 18:14:38 +0700

    Sửa lại download khi hiển thị video

--- DownloadYoutubeVideo/download/views.py
+++ DownloadYoutubeVideo/download/views.py
@@ -7,40 +7,54 @@ from pytube.exceptions import RegexMatchError
 def index(request):
     return render(request, 'index.html')
 def getVideo(request):
-    link = (request.POST.get('link')).strip()
-    try:
-        video = YouTube(link)
-        if(video.length):
+    if request.method == 'POST':
+        link = (request.POST.get('link')).strip()
+        try:
+            video = YouTube(link)
+            if(video.length):
+                return JsonResponse({
+                    'link':link,
+                    'status':200
+                })
+            else:
+                return JsonResponse({
+                'status':404,
+                'message':'Không lấy được link'
+                })
+        except RegexMatchError:
             return JsonResponse({
-                'link':link
+                'status':404,
+            'message':'Không lấy được link'
+            })
+    else:
+        return JsonResponse({
+           'status':400,
+           'message':'Method sai'
+        })
+def Download(request):
+    if request.method == 'POST':
+        link = (request.POST.get('link')).strip()
+        if(link):
+            video = YouTube(link)
+            # stream = video.streams.get_lowest_resolution()
+            stream = video.streams.get_by_resolution("720p")
+            stream.download()
+            return JsonResponse({
+                'length': video.length, #Lấy độ dài
+                'title': video.title, #lấy tiêu đề
+                'views': video.views, #Lấy số view
+                'author': video.author, #lấy tác giả
+                'link':link,
+                'status':'success',
+                'message':'Successfully downloaded'
             })
         else:
             return JsonResponse({
-               'status':404,
-               'message':'Không lấy được link'
+                'link': link,
+                'message':'Không lấy được link khi download'
             })
-    except RegexMatchError:
-        return JsonResponse({
-            'status':404,
-           'message':'Không lấy được link'
-        })
-def Download(request):
-    link =json.dumps(getVideo(request)['link']) #Chuyển json thành string
-    if(link):
-        video = YouTube(link)
-        # stream = video.streams.get_lowest_resolution()
-        stream = video.streams.get_by_resolution("720p")
-        stream.download()
-        return JsonResponse({
-            'length': video.length, #Lấy độ dài
-            'title': video.title, #lấy tiêu đề
-            'views': video.views, #Lấy số view
-            'author': video.author, #lấy tác giả
-            'link':link,
-            'status':'success',
-            'message':'Successfully downloaded'
-        })
     else:
         return JsonResponse({
-            'message':'Không lấy được link'
+           'status':400,
+           'message':'Method sai'
         })
